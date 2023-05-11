3144a1372c5a4491e906d8ec5e30939a65ca784a
kine <bluekine78@gmail.com>
2023-05-11 12:01:51 +0900

    Initial Commit

---
+++ sample/sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleApp.swift
+//  sample
+//
+//  Created by Kain on 2023/05/11.
+//
+
+import SwiftUI
+
+@main
+struct sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
